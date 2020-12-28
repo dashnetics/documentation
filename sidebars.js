7805bb59277b917c825224c155a54100c435bbb1
damoncarley <damon.carley@4d.com>
2020-12-29 08:38:00 +1100

    mailchimp title change and field types page

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['gettingStarted/main'],
-    Features: ['features/bracketnotation','features/mailchimp','features/selling','features/formactions'],
+    Features: ['features/bracketnotation','features/fieldtypes','features/formactions','features/mailchimp','features/selling'],
     API: ['api']
   },
 };
