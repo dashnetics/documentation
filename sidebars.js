1df35f62948bbeb7d213b6b3bb796653cc282537
Hikansh kapoor <hikanshk@gmail.com>
2021-01-25 11:35:24 +1100

    Added an extra level inside features

--- sidebars.js
+++ sidebars.js
@@ -17,12 +17,19 @@ module.exports = {
       'features/mailchimp',
       'features/selling',
       'features/stripe',
+      {
+        'Add HTML and CSS': [
+          'addingHTMLandCSS/addHTML-main',
+          'addingHTMLandCSS/addHTML',
+          'addingHTMLandCSS/addCSS',
+        ],
+      },
     ],
     API: ['api'],
-    'Add HTML and CSS': [
-      'addingHTMLandCSS/addHTML-main',
-      'addingHTMLandCSS/addHTML',
-      'addingHTMLandCSS/addCSS',
-    ],
+    // 'Add HTML and CSS': [
+    //   'addingHTMLandCSS/addHTML-main',
+    //   'addingHTMLandCSS/addHTML',
+    //   'addingHTMLandCSS/addCSS',
+    // ],
   },
 };
