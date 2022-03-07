8d8b3ac5108243d628ba7e5619deb33440a325f0
Dominic Collier <techsupport@icsr.com.au>
2022-03-07 15:37:57 +1100

    Revert "new layout"
    This reverts commit 1acc37de7be06e9844d7a243e5236f886686fa4b.

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,91 +5,56 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-        title: 'Start in minutes',
+        title: 'Video Tutorials',
         imageUrl: 'img/road-start.jpg',
         addr: () => {
             window.location.href = "./docs/tutorials/tutorials-intro"
         },
     description: (
       <>
-            <i className="material-icons color">videocam</i> Video Tutorials and Articles designed from the ground up to be easily understood and
-            used to get you up and running quickly.
+            <i className="material-icons color">videocam</i> Video Presentations and Articles designed from the ground up to be easily understood and
+            used to get your website up and running quickly.
             
       </>
     ),
   },
    {
-        title: 'No Code Solution',
-        imageUrl: 'img/no-code.jpg',
+        title: 'User Guides and Manuals',
+        imageUrl: 'img/manuals.jpg',
         addr: () => {
             window.location.href = "./docs/manuals/manuals-intro"
         },
         description: (
             <>
-                We write the code so you don't have to, our platform is designed to be used by anyone on your staff with no development knowledge.
+                All your helpful manuals and user documents in one place, you can even create your own, and we&apos;ll do the chores. Go
+                ahead and move your documents into the docs directory.
             </>
         ),
     },
     {
-        title: 'Designed for Mobile, Tablet and Desktop',
-        imageUrl: 'img/mobile-first.jpg',
+        title: 'Features and Highlights',
+        imageUrl: 'img/road-features.jpg',
         addr: () => {
             window.location.href = "./docs/features/features-intro"
         },
         description: (
             <>
-                Create beautiful web pages for your mobile, tablet and desktop visitors, regardless of operating system or browser
+                Browse through some of the features and discover what you can do with the world's most powerful automation tool
             </>
         ),
     },
      {
-        title: 'Create your online store and take payments',
-        imageUrl: 'img/creditcard.jpg',
+        title: 'Connecting other Applications',
+        imageUrl: 'img/road-api.jpg',
         addr: () => {
             window.location.href = "http://dashnetics.com.au"
         },
         description: (
             <>
-                Create or Import your items pricing, pictures into an online store.
+                For Advanced Users / Development Teams, learn how to connect Dashnetics through APIs. 
             </>
         ),
     },
-    {
-                     title: 'Easily manage your products and pricing',
-                     imageUrl: 'img/store.jpg',
-                     addr: () => {
-                     window.location.href = "http://dashnetics.com.au"
-                     },
-                     description: (
-                                   <>
-                                   Change your items, or create new campaigns easily with Dashnetics.
-                                   </>
-                                   ),
-                     },
-                     {
-                     title: 'Manage Memberships & Subcriptions',
-                     imageUrl: 'img/memberships.jpg',
-                     addr: () => {
-                     window.location.href = "http://dashnetics.com.au"
-                     },
-                     description: (
-                                   <>
-                                   Easily and automatically manage your memberships, including renewals, fees, discounts, data and more.
-                                   </>
-                                   ),
-                     },
-                     {
-                     title: 'Create and automate powerful marketing campaigns and mailings',
-                     imageUrl: 'img/mailchimp-certified-partner.png',
-                     addr: () => {
-                     window.location.href = "http://dashnetics.com.au"
-                     },
-                     description: (
-                                   <>
-                                   Easily and automatically manage your mailing lists as well as mail & marketing content, frequency, audience and responses.
-                                   </>
-                                   ),
-                     },
 ];
 
 function Feature({ imageUrl, Svg, title, description, addr }) {
